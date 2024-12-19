6d3d16f42006805c9e4815f74c3036c226b2136d
coded_coder <sholay048@gmail.com>
2024-12-19 01:07:15 +0100

    I reduced the requirements.txt and corrected the cors allowed origins

--- webbase/settings.py
+++ webbase/settings.py
@@ -194,6 +194,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://192.168.208.1:3000"
+    "http://192.168.208.1:3000",
     "https://webfacebackend.onrender.com"
 ]
